bb90438e332c2c2b759e29e45857a0144f1314cd
cfh463 <rob.galvin@motorolasolutions.com>
2013-11-15 12:35:42 -0500

    Index actions update qty field

--- public/js/routes/product.js
+++ public/js/routes/product.js
@@ -1,6 +1,16 @@
 App.ProductsIndexRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('product');
+  },
+  actions: {
+    up: function(m) {
+      m.set('qty',parseInt(m.get('qty')) + 1);
+      m.save();
+    },
+    down: function(m) {
+      m.set('qty',parseInt(m.get('qty')) - 1);
+      m.save();
+    }
   }
   
 });
